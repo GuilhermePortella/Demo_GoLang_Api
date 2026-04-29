7304c8afa6e07232cd9854dbbba9e6589312cbb6
Guilherme Portella <guilhermeportella2@gmail.com>
2026-04-28 21:47:02 -0300

    criando um post com a doc oficial

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -6,6 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
+}
+
 // album represents data about a record album.
 type album struct {
 	ID     string  `json:"id"`
@@ -21,14 +29,23 @@ var albums = []album{
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
-func main() {
-	router := gin.Default()
-	router.GET("/albums", getAlbums)
-
-	router.Run("localhost:8080")
-}
-
 // getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+
 }
